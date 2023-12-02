c40b9a50dd538a7225d5637d15698c973582bf0b
Trung Kien <nguyntrungkin091@gmail.com>
2023-12-02 23:03:39 +0700

    update logic login, register

--- app/src/main/java/com/example/flashcardapplication/LoginActivity.kt
+++ app/src/main/java/com/example/flashcardapplication/LoginActivity.kt
@@ -1,11 +1,158 @@
 package com.example.flashcardapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.marginEnd
+import androidx.core.view.marginStart
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthInvalidUserException
+import com.google.firebase.firestore.FirebaseFirestore
 
+@Suppress("NAME_SHADOWING")
 class LoginActivity : AppCompatActivity() {
+    private var auth: FirebaseAuth? = null
+    private var database: FirebaseFirestore? = null
+
+    private var edEmail : TextInputEditText? = null
+    private var edPassword: TextInputEditText? = null
+    private var layoutEmail: TextInputLayout? = null
+    private var layoutPassword: TextInputLayout? = null
+    private var tvForgotPassword: TextView? = null
+    private var btnLogin: Button? = null
+    private var tvRegister: TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        auth = FirebaseAuth.getInstance()
+        database = FirebaseFirestore.getInstance()
+
+        edEmail = findViewById(R.id.ed_email)
+        edPassword = findViewById(R.id.ed_password)
+        layoutEmail = findViewById(R.id.layout_email)
+        layoutPassword = findViewById(R.id.layout_password)
+        tvForgotPassword = findViewById(R.id.tv_forgot_password)
+        btnLogin = findViewById(R.id.btn_login)
+        tvRegister = findViewById(R.id.tv_register)
+
+        tvForgotPassword?.movementMethod = android.text.method.LinkMovementMethod.getInstance()
+        tvForgotPassword?.setOnClickListener {
+            // create edittext
+            val et = EditText(this)
+            et.hint = "Enter your email address"
+            val params = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            )
+            params.marginStart = 16 * resources.displayMetrics.density.toInt()
+            params.marginEnd = 16 * resources.displayMetrics.density.toInt()
+            et.layoutParams = params
+
+            val builder = AlertDialog.Builder(this)
+                .setMessage("Please enter your email address")
+                .setView(et)
+                .setPositiveButton("OK") { _, _ ->
+                    // method to send new password via email
+                    val email = et.text.toString()
+                    if (email.isEmpty()) {
+                        Toast.makeText(this, "Please enter your email address", Toast.LENGTH_SHORT).show()
+                    } else {
+                        auth?.sendPasswordResetEmail(email)
+                            ?.addOnCompleteListener { task ->
+                                if (task.isSuccessful) {
+                                    Toast.makeText(this, "Email was sent", Toast.LENGTH_SHORT).show()
+                                } else {
+                                    Toast.makeText(this, "Email not sent", Toast.LENGTH_SHORT).show()
+                                }
+                            }
+                    }
+                }.setNegativeButton("Cancel") { dialog, _ ->
+                    dialog.dismiss()
+                }
+
+            builder.show()
+        }
+
+        tvRegister?.movementMethod = android.text.method.LinkMovementMethod.getInstance()
+        tvRegister?.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        btnLogin?.setOnClickListener {
+            val email = edEmail?.text.toString()
+            val password = edPassword?.text.toString()
+            login(email, password)
+        }
+    }
+
+    private fun login(email: String, password: String) {
+        if (email.isEmpty() && layoutEmail?.isErrorEnabled == false) {
+            layoutEmail?.error = "Please enter email address"
+            layoutEmail?.isErrorEnabled = true
+            return
+        } else {
+            layoutEmail?.isErrorEnabled = false
+        }
+
+        if (password.isEmpty() && layoutPassword?.isErrorEnabled == false) {
+            layoutPassword?.error = "Please enter password"
+            layoutPassword?.isErrorEnabled = true
+            return
+        } else {
+            layoutPassword?.isErrorEnabled = false
+        }
+
+        auth?.signInWithEmailAndPassword(email, password)
+            ?.addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = auth?.currentUser
+                    val uid = user?.uid
+                    val email = user?.email
+                    val name = user?.displayName
+                    val photoUrl = user?.photoUrl
+
+                    val userMap = hashMapOf(
+                        "uid" to uid,
+                        "email" to email,
+                        "name" to name,
+                        "photoUrl" to photoUrl
+                    )
+
+                    database?.collection("users")?.document(uid!!)?.set(userMap)
+
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    val exception = task.exception
+                    when (exception) {
+                        is FirebaseAuthInvalidUserException -> {
+                            layoutEmail?.error = "Email not found"
+                            layoutEmail?.isErrorEnabled = true
+                        }
+
+                        is FirebaseAuthInvalidCredentialsException -> {
+                            layoutPassword?.error = "Wrong password"
+                            layoutPassword?.isErrorEnabled = true
+                        }
+
+                        else -> {
+                            Toast.makeText(this, "Login failed", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+            }
     }
 }
--- app/src/main/java/com/example/flashcardapplication/SignUpActivity.kt
+++ app/src/main/java/com/example/flashcardapplication/SignUpActivity.kt
@@ -2,10 +2,113 @@ package com.example.flashcardapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class SignUpActivity : AppCompatActivity() {
+    private var auth: FirebaseAuth? = null
+    private var database: FirebaseFirestore? = null
+
+    private var edEmail : TextInputEditText? = null
+    private var edPassword: TextInputEditText? = null
+    private var edRePassword: TextInputEditText? = null
+    private var layoutEmail: TextInputLayout? = null
+    private var layoutPassword: TextInputLayout? = null
+    private var layoutRePassword: TextInputLayout? = null
+    private var btnRegister: Button? = null
+    private var tvLogin: TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        auth = FirebaseAuth.getInstance()
+        database = FirebaseFirestore.getInstance()
+
+        edEmail = findViewById(R.id.ed_email)
+        edPassword = findViewById(R.id.ed_password)
+        edRePassword = findViewById(R.id.ed_re_password)
+        layoutEmail = findViewById(R.id.layout_email)
+        layoutPassword = findViewById(R.id.layout_password)
+        layoutRePassword = findViewById(R.id.layout_re_password)
+        btnRegister = findViewById(R.id.btn_register)
+        tvLogin = findViewById(R.id.tv_login)
+
+        tvLogin?.movementMethod = android.text.method.LinkMovementMethod.getInstance()
+        tvLogin?.setOnClickListener {
+            val intent = android.content.Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        btnRegister?.setOnClickListener {
+            val email = edEmail?.text.toString()
+            val password = edPassword?.text.toString()
+            val rePassword = edRePassword?.text.toString()
+            register(email, password, rePassword)
+        }
+    }
+
+    private fun register(email: String, password: String, rePassword: String) {
+        if(validateEmail(email)
+            && validatePassword(password)
+            && validateRePassword(password, rePassword)) {
+            auth?.createUserWithEmailAndPassword(email, password)
+                ?.addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        val user = auth?.currentUser
+                        val intent = android.content.Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    } else {
+                        when (task.exception) {
+                            is com.google.firebase.auth.FirebaseAuthWeakPasswordException -> {
+                                layoutPassword?.error = "Password is too weak"
+                            }
+                            is com.google.firebase.auth.FirebaseAuthInvalidCredentialsException -> {
+                                layoutEmail?.error = "Invalid email"
+                            }
+                            is com.google.firebase.auth.FirebaseAuthUserCollisionException -> {
+                                layoutEmail?.error = "Email already registered"
+                            }
+                            else -> {
+                                Toast.makeText(this, "Registration failed", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    }
+                }
+        }
+    }
+
+    private fun validateEmail(email: String): Boolean {
+            return if (email.isEmpty()) {
+                layoutEmail?.error = "Email cannot be empty"
+                false
+            } else {
+                layoutEmail?.error = null
+                true
+            }
+    }
+    private fun validatePassword(password: String): Boolean {
+            return if (password.length < 6) {
+                layoutPassword?.error = "Password is too short"
+                false
+            } else {
+                layoutPassword?.error = null
+                true
+            }
+    }
+    private fun validateRePassword(password: String, rePassword: String): Boolean {
+            return if (password != rePassword) {
+                layoutRePassword?.error = "Password does not match"
+                false
+            } else {
+                layoutRePassword?.error = null
+                true
+            }
     }
 }
