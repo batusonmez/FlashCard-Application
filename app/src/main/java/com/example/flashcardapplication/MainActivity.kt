2f1d21681279e3c8333564500c0f1bedd700d053
Trung Kien <nguyntrungkin091@gmail.com>
2023-12-10 20:24:35 +0700

    update pri-view homepage

--- app/src/main/java/com/example/flashcardapplication/MainActivity.kt
+++ app/src/main/java/com/example/flashcardapplication/MainActivity.kt
@@ -1,11 +1,61 @@
+@file:Suppress("DEPRECATION")
+
 package com.example.flashcardapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager.widget.ViewPager
+import com.example.flashcardapplication.fragments.HomePageFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private var viewPage: ViewPager? = null
+    private var bottomNavigation : BottomNavigationView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewPage = findViewById(R.id.view_page)
+        bottomNavigation = findViewById(R.id.bottom_navigation)
+
+        val adapter = ViewPagerAdapter(supportFragmentManager)
+        adapter.addFragment(HomePageFragment(), "Trang chủ")
+
+
+        viewPage?.adapter = adapter
+
+
+        bottomNavigation?.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.i_trang_chu -> viewPage?.currentItem = 0
+                R.id.i_loi_giai -> viewPage?.currentItem = 1
+                R.id.i_add -> viewPage?.currentItem = 2
+                R.id.i_thu_vien -> viewPage?.currentItem = 3
+                R.id.i_ho_so -> viewPage?.currentItem = 4
+            }
+            true
+        }
+    }
+
+}
+
+class ViewPagerAdapter(manager: FragmentManager) : FragmentPagerAdapter(manager) {
+    private val fragmentList = ArrayList<Fragment>()
+    private val fragmentTitleList = ArrayList<String>()
+    override fun getItem(position: Int): Fragment {
+        return fragmentList[position]
+    }
+    override fun getCount(): Int {
+        return fragmentList.size
+    }
+    fun addFragment(fragment: Fragment, title: String) {
+        fragmentList.add(fragment)
+        fragmentTitleList.add(title)
+    }
+    override fun getPageTitle(position: Int): CharSequence {
+        return fragmentTitleList[position]
     }
 }
--- app/src/main/java/com/example/flashcardapplication/fragments/HomePageFragment.kt
+++ app/src/main/java/com/example/flashcardapplication/fragments/HomePageFragment.kt
@@ -1,60 +1,143 @@
 package com.example.flashcardapplication.fragments
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.example.flashcardapplication.R
+import com.example.flashcardapplication.databinding.FragmentHomePageBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.squareup.picasso.Picasso
+import de.hdodenhof.circleimageview.CircleImageView
+import java.io.Serializable
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HomePageFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomePageFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var auth : FirebaseAuth? = null
+    private var database : FirebaseFirestore? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var dataCourse: ArrayList<Data>? = null
+    private var dataFolder: ArrayList<Data>? = null
 
+    @SuppressLint("SetTextI18n")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_page, container, false)
+    ): View {
+        auth = FirebaseAuth.getInstance()
+        database = FirebaseFirestore.getInstance()
+
+        val binding = FragmentHomePageBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        val currentUser = auth?.currentUser
+        if(currentUser != null) {
+            Picasso.get().load(currentUser.photoUrl).into(binding.ivAvatar)
+            binding.tvHelloUser.text = "Xin chào, ${currentUser.displayName}"
+        }
+
+        binding.edtSearch.setOnClickListener {
+            // handle later
+        }
+
+        binding.tvAllCourse.movementMethod = android.text.method.LinkMovementMethod.getInstance()
+        // redirect to all course in layout library
+
+        dataCourse = ArrayList()
+        // to test
+        dataCourse?.add(Data().apply {
+            name = "Lập trình Android"
+            numberLesson = 10
+            avatar = Uri.parse("android.resource://com.example.flashcardapplication/drawable/avatar")
+            nameAuthor = "Nguyễn Văn A"
+        })
+        dataCourse?.add(Data().apply {
+            name = "Lập trình Android"
+            numberLesson = 10
+            avatar = Uri.parse("android.resource://com.example.flashcardapplication/drawable/avatar")
+            nameAuthor = "Nguyễn Văn A"
+        })
+
+        dataCourse?.let {
+            binding.rcvAllCourse.adapter = DataAdapter(context, it)
+        }
+        binding.rcvAllCourse.layoutManager = androidx.recyclerview.widget.LinearLayoutManager(
+            context,
+            androidx.recyclerview.widget.LinearLayoutManager.HORIZONTAL,
+            false
+        )
+
+        binding.tvAllFolder.movementMethod = android.text.method.LinkMovementMethod.getInstance()
+        // redirect to all folder in layout library
+
+
+        dataFolder = ArrayList()
+        // to test
+        dataFolder?.add(Data().apply {
+            name = "Lập trình Android"
+            numberLesson = 10
+            avatar = Uri.parse("android.resource://com.example.flashcardapplication/drawable/avatar")
+            nameAuthor = "Nguyễn Văn A"
+        })
+        dataFolder?.add(Data().apply {
+            name = "Lập trình Android"
+            numberLesson = 10
+            avatar = Uri.parse("android.resource://com.example.flashcardapplication/drawable/avatar")
+            nameAuthor = "Nguyễn Văn A"
+        })
+
+        dataFolder?.let {
+            binding.rcvAllFolder.adapter = DataAdapter(context, it)
+        }
+        binding.rcvAllFolder.layoutManager = androidx.recyclerview.widget.LinearLayoutManager(
+            context,
+            androidx.recyclerview.widget.LinearLayoutManager.HORIZONTAL,
+            false
+        )
+
+        return view
+    }
+}
+
+class Data : Serializable {
+    var name: String? = null
+    var numberLesson: Int? = null
+    var avatar: Uri? = null
+    var nameAuthor: String? = null
+}
+
+class DataAdapter(
+    private val context: Context?,
+    private val data: ArrayList<Data>) :
+    RecyclerView.Adapter<DataAdapter.DataViewHolder>() {
+    class DataViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val tvNameCourse : TextView? = itemView.findViewById(R.id.tv_nameCourse)
+        val tvNumberLesson : TextView? = itemView.findViewById(R.id.tv_numberLesson)
+        val civAvatar : CircleImageView? = itemView.findViewById(R.id.civ_avatar)
+        val tvNameAuthor : TextView? = itemView.findViewById(R.id.tv_nameAuthor)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomePageFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomePageFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DataViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.item_course, parent, false)
+        return DataViewHolder(view)
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onBindViewHolder(holder: DataViewHolder, position: Int) {
+        val item = data[position]
+        holder.tvNameCourse?.text = item.name
+        holder.tvNumberLesson?.text = item.numberLesson.toString() + " thuật ngữ"
+        holder.civAvatar?.setImageURI(item.avatar)
+        holder.tvNameAuthor?.text = item.nameAuthor
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
     }
 }
