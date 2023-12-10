a9aed4912bdc922a82f4615a7d4a738f4dc2994a
Trung Kien <nguyntrungkin091@gmail.com>
2023-12-10 20:53:52 +0700

    update bottom-sheet

--- app/src/main/java/com/example/flashcardapplication/MainActivity.kt
+++ app/src/main/java/com/example/flashcardapplication/MainActivity.kt
@@ -4,12 +4,16 @@ package com.example.flashcardapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import androidx.viewpager.widget.ViewPager
 import com.example.flashcardapplication.fragments.HomePageFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class MainActivity : AppCompatActivity() {
     private var viewPage: ViewPager? = null
@@ -32,7 +36,10 @@ class MainActivity : AppCompatActivity() {
             when (item.itemId) {
                 R.id.i_trang_chu -> viewPage?.currentItem = 0
                 R.id.i_loi_giai -> viewPage?.currentItem = 1
-                R.id.i_add -> viewPage?.currentItem = 2
+                R.id.i_add -> {
+                    val bottomSheetFragment = CustomBottomSheetDialogFragment()
+                    bottomSheetFragment.show(supportFragmentManager, bottomSheetFragment.tag)
+                }
                 R.id.i_thu_vien -> viewPage?.currentItem = 3
                 R.id.i_ho_so -> viewPage?.currentItem = 4
             }
@@ -57,5 +64,15 @@ class ViewPagerAdapter(manager: FragmentManager) : FragmentPagerAdapter(manager)
     }
     override fun getPageTitle(position: Int): CharSequence {
         return fragmentTitleList[position]
+    }
+}
+
+class CustomBottomSheetDialogFragment : BottomSheetDialogFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.bottom_sheet_dialog, container, false)
     }
 }
