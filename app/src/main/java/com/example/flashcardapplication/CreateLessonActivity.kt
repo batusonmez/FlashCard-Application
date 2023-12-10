4c8d670fc0e6d51c17f78c4a27bb02d78ce0d587
Trung Kien <nguyntrungkin091@gmail.com>
2023-12-11 02:35:57 +0700

    update

--- app/src/main/java/com/example/flashcardapplication/CreateLessonActivity.kt
+++ app/src/main/java/com/example/flashcardapplication/CreateLessonActivity.kt
@@ -1,15 +1,136 @@
 package com.example.flashcardapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
 import com.example.flashcardapplication.databinding.ActivityCreateLessonBinding
+import java.io.Serializable
 
 class CreateLessonActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCreateLessonBinding
+    private var data: ArrayList<Terminology>? = null
+    @SuppressLint("NotifyDataSetChanged")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_create_lesson)
 
+        supportActionBar?.title = "Tạo học phần"
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val topic = binding.edtTopic.text.toString()
+
+        binding.tvScan.movementMethod = android.text.method.LinkMovementMethod.getInstance()
+        // handle to scan file excel
+
+        binding.tvDescription.movementMethod = android.text.method.LinkMovementMethod.getInstance()
+        // handle to open dialog to input description
+
+        // default to add 2 terminology
+        data = ArrayList()
+        data?.add(Terminology().apply {
+            terminology = ""
+            definition = ""
+        })
+        data?.add(Terminology().apply {
+            terminology = ""
+            definition = ""
+        })
+
+        data?.let {
+            binding.rcvCreateLesson.adapter = CreateLessonAdapter(this, it)
+        }
+        binding.rcvCreateLesson.layoutManager =
+            androidx.recyclerview.widget.LinearLayoutManager(this)
+
+        binding.btnCreateLesson.setOnClickListener {
+            data?.add(Terminology().apply {
+                terminology = ""
+                definition = ""
+            })
+            binding.rcvCreateLesson.adapter?.notifyDataSetChanged()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.create_lesson_option, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.i_save -> {
+                val adapter = binding.rcvCreateLesson.adapter as CreateLessonAdapter
+                val data = adapter.getTerminologyData()
+                // handle to save data to database
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
+
+class Terminology: Serializable {
+    var terminology: String? = null
+    var definition: String? = null
+}
+
+class CreateLessonAdapter(
+    private val context: Context,
+    private val data: ArrayList<Terminology>) :
+    RecyclerView.Adapter<CreateLessonAdapter.ViewHolder>() {
+
+    fun getTerminologyData(): ArrayList<Terminology> {
+        return data
+    }
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val edtTerminology: EditText? = view.findViewById(R.id.edt_terminology)
+        val edtDefinition: EditText? = view.findViewById(R.id.edt_definition)
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.item_create_lesson, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = data[position]
+        holder.edtTerminology?.setText(item.terminology)
+        holder.edtDefinition?.setText(item.definition)
+
+        holder.edtTerminology?.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                item.terminology = s.toString()
+            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // do nothing
+            }
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // do nothing
+            }
+        })
+        holder.edtDefinition?.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                item.definition = s.toString()
+            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // do nothing
+            }
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // do nothing
+            }
+        })
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
     }
 }
--- app/src/main/java/com/example/flashcardapplication/MainActivity.kt
+++ app/src/main/java/com/example/flashcardapplication/MainActivity.kt
@@ -2,49 +2,46 @@
 
 package com.example.flashcardapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
-import androidx.viewpager.widget.ViewPager
-import com.example.flashcardapplication.fragments.HomePageFragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.example.flashcardapplication.databinding.ActivityMainBinding
+import com.example.flashcardapplication.fragments.HomePageFragment
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private var viewPage: ViewPager? = null
-    private var bottomNavigation : BottomNavigationView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-
-        viewPage = binding.viewPager
-        bottomNavigation = binding.bottomNavigation
 
         val adapter = ViewPagerAdapter(supportFragmentManager)
         adapter.addFragment(HomePageFragment(), "Trang chủ")
 
 
-        viewPage?.adapter = adapter
+        binding.viewPager.adapter = adapter
 
 
-        bottomNavigation?.setOnNavigationItemSelectedListener { item ->
+        binding.bottomNavigation.setOnNavigationItemSelectedListener { item ->
             when (item.itemId) {
-                R.id.i_trang_chu -> viewPage?.currentItem = 0
-                R.id.i_loi_giai -> viewPage?.currentItem = 1
+                R.id.i_trang_chu -> binding.viewPager.currentItem = 0
+                R.id.i_loi_giai -> binding.viewPager.currentItem = 1
                 R.id.i_add -> {
                     val bottomSheetFragment = CustomBottomSheetDialogFragment()
                     bottomSheetFragment.show(supportFragmentManager, bottomSheetFragment.tag)
                 }
-                R.id.i_thu_vien -> viewPage?.currentItem = 3
-                R.id.i_ho_so -> viewPage?.currentItem = 4
+
+                R.id.i_thu_vien -> binding.viewPager.currentItem = 3
+                R.id.i_ho_so -> binding.viewPager.currentItem = 4
             }
             true
         }
@@ -71,13 +68,38 @@ class ViewPagerAdapter(manager: FragmentManager) : FragmentPagerAdapter(manager)
 }
 
 class CustomBottomSheetDialogFragment : BottomSheetDialogFragment() {
+    private var tvCourse: TextView? = null
+    private var tvFolder: TextView? = null
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.bottom_sheet_dialog, container, false)
+        val view = inflater.inflate(R.layout.bottom_sheet_dialog, container, false)
 
+        tvCourse = view.findViewById(R.id.tv_course)
+        tvFolder = view.findViewById(R.id.tv_folder)
 
+        tvCourse?.movementMethod = android.text.method.LinkMovementMethod.getInstance()
+        tvCourse?.setOnClickListener {
+            startActivity(Intent(activity, CreateLessonActivity::class.java))
+        }
+
+        tvFolder?.movementMethod = android.text.method.LinkMovementMethod.getInstance()
+        tvFolder?.setOnClickListener {
+            AlertDialog.Builder(MainActivity())
+                .setTitle("Tạo thư mục")
+                .setView(R.layout.custom_view_dialog)
+                .setPositiveButton("Ok") { dialog, _ ->
+                    dialog.dismiss()
+                }
+                .setNegativeButton("Hủy") { dialog, _ ->
+                    dialog.dismiss()
+                }
+                .create()
+                .show()
+        }
+
+        return view
     }
 }
